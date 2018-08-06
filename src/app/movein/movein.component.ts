a5f42d1f0b86a6d6b2c122918520a28669172f9e
A231049 <simthembile.yuku@standardbank.co.za>
2018-08-06 11:41:30 +0200

    MI-0001 [Terra] Adding project

---
+++ src/app/movein/movein.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movein',
+  templateUrl: './movein.component.html',
+  styleUrls: ['./movein.component.css']
+})
+export class MoveinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/unemployed-counter/unemployed-counter.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Counter } from '../Counter';
+
+@Component({
+  selector: 'app-unemployed-counter',
+  templateUrl: './unemployed-counter.component.html',
+  styleUrls: ['./unemployed-counter.component.css']
+})
+export class UnemployedCounterComponent implements OnInit {
+
+  count: Counter = {
+  unemployedCount: 1000000000
+};
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

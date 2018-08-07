007c69c3084f9548696de863cd26834f281a259c
A231049 <simthembile.yuku@standardbank.co.za>
2018-08-07 21:17:31 +0200

    [Terra] Fixing lint failures

--- src/app/movein/movein.component.spec.ts
+++ src/app/movein/movein.component.spec.ts
@@ -24,11 +24,4 @@ describe('MoveinComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should have message', () => {
-    const moveinComponent = fixture.debugElement.componentInstance;
-    const movein = 'The MOVEIN platform is a platform that aims in equipping and directing the unemployed individuals to specific materials ' +
-      'that are market and industry relevant at that point in time for their career of choice. This platform is an information ' +
-      'centered platform to guide and provide online training assessments to unemployed individuals.';
-    expect(moveinComponent.movein).toEqual(movein);
-  });
 });
--- src/app/movein/movein.component.ts
+++ src/app/movein/movein.component.ts
@@ -7,11 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoveinComponent implements OnInit {
   movein: string;
-  constructor() {
-    this.movein = 'The MOVEIN platform is a platform that aims in equipping and directing the unemployed individuals to specific materials ' +
-    'that are market and industry relevant at that point in time for their career of choice. This platform is an information ' +
-    'centered platform to guide and provide online training assessments to unemployed individuals.';
-  }
+  constructor() { }
 
   ngOnInit() {
   }

0de8e9f7dfa5140032c6a30ec4a21d96aaf5cdcc
A231049 <simthembile.yuku@standardbank.co.za>
2018-08-07 20:52:11 +0200

    [Terra] Styling movein component

--- src/app/movein/movein.component.ts
+++ src/app/movein/movein.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movein.component.css']
 })
 export class MoveinComponent implements OnInit {
-
-  constructor() { }
+  movein: string;
+  constructor() {
+    this.movein = 'The MOVEIN platform is a platform that aims in equipping and directing the unemployed individuals to specific materials ' +
+    'that are market and industry relevant at that point in time for their career of choice. This platform is an information ' +
+    'centered platform to guide and provide online training assessments to unemployed individuals.';
+  }
 
   ngOnInit() {
   }

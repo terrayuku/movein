7e1a75d49d94d9293335746da697bc50f0418551
A231049 <simthembile.yuku@standardbank.co.za>
2018-08-10 00:23:26 +0200

    [Terra] Update profile screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PreRegistrationComponent } from './pre-registration/pre-registration.component';
 import { SignupComponent } from './signup/signup.component';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
 
 const routes: Routes = [
   { path: 'register', component: PreRegistrationComponent },
-  { path: 'signin', component: SignupComponent }
+  { path: 'signin', component: SignupComponent },
+  { path: 'update-profile', component: UpdateProfileComponent }
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],

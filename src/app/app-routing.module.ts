41a805d0477b410d177c5df8ef51770332b5d45c
A231049 <simthembile.yuku@standardbank.co.za>
2018-08-12 15:17:40 +0200

    [Terra] fixing home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { PreRegistrationComponent } from './pre-registration/pre-registration.component';
 import { SignupComponent } from './signup/signup.component';
 import { UpdateProfileComponent } from './update-profile/update-profile.component';
-import {DashboardComponent} from "./dashboard/dashboard.component";
-import {AppComponent} from "./app.component";
-import {HomeComponent} from "./home/home.component";
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'register', component: PreRegistrationComponent, name: 'PreRegister' },
-  { path: 'signin', component: SignupComponent, name: 'SignIp' },
-  { path: 'update-profile', component: UpdateProfileComponent, name: 'UpdateProfile' },
-  { path: 'dashboard', component: DashboardComponent, name: 'Dashboard' }
+  { path: 'register', component: PreRegistrationComponent },
+  { path: 'signin', component: SignupComponent },
+  { path: 'update-profile', component: UpdateProfileComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],

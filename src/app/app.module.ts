505f861f2d6e41c662746415198c47bbb76d2da6
A231049 <simthembile.yuku@standardbank.co.za>
2018-08-12 12:42:13 +0200

    [Terra] Adding Dasboard Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material';
-import {MatGridListModule} from '@angular/material';
-import {MatButtonModule} from '@angular/material';
-import {MatDialogModule} from '@angular/material';
-import {FormsModule} from '@angular/forms';
+// import {MatCardModule} from '@angular/material';
+// import {MatGridListModule} from '@angular/material';
+// import {MatButtonModule} from '@angular/material';
+// import {MatDialogModule} from '@angular/material';
+// import {FormsModule} from '@angular/forms';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -25,6 +25,46 @@ import { UpdateProfileComponent } from './update-profile/update-profile.componen
 
 import { environment } from '../environments/environment';
 import {AuthService} from './auth.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -35,7 +75,8 @@ import {AuthService} from './auth.service';
     RegisterComponent,
     PreRegistrationComponent,
     SignupComponent,
-    UpdateProfileComponent
+    UpdateProfileComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -48,8 +89,47 @@ import {AuthService} from './auth.service';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
+  // exports: [
+  //   MatAutocompleteModule,
+  //   MatBadgeModule,
+  //   MatBottomSheetModule,
+  //   MatButtonModule,
+  //   MatButtonToggleModule,
+  //   MatCardModule,
+  //   MatCheckboxModule,
+  //   MatChipsModule,
+  //   MatStepperModule,
+  //   MatDatepickerModule,
+  //   MatDialogModule,
+  //   MatDividerModule,
+  //   MatExpansionModule,
+  //   MatGridListModule,
+  //   MatIconModule,
+  //   MatInputModule,
+  //   MatListModule,
+  //   MatMenuModule,
+  //   MatNativeDateModule,
+  //   MatPaginatorModule,
+  //   MatProgressBarModule,
+  //   MatProgressSpinnerModule,
+  //   MatRadioModule,
+  //   MatRippleModule,
+  //   MatSelectModule,
+  //   MatSidenavModule,
+  //   MatSliderModule,
+  //   MatSlideToggleModule,
+  //   MatSnackBarModule,
+  //   MatSortModule,
+  //   MatTableModule,
+  //   MatTabsModule,
+  //   MatToolbarModule,
+  //   MatTooltipModule,
+  //   MatTreeModule,
+  // ],
   providers: [AuthService, AngularFireAuth],
   bootstrap: [AppComponent],
   schemas: [ NO_ERRORS_SCHEMA ]
